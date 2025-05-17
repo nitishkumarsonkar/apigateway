d26bffb9c20c4bf0a4f4672db587d6f4d500139b
Nitish Sonkar <sonkarnitish70@gmail.com>
2025-05-17 22:09:20 +0530

    first commit

---
+++ src/main/java/com/example/apigateway/route/InventoryServiceRoutes.java
@@ -0,0 +1,28 @@
+package com.example.apigateway.route;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.*;
+
+@Configuration
+public class InventoryServiceRoutes {
+    @Bean
+    RouterFunction<ServerResponse> inventoryRoutes(){
+        return GatewayRouterFunctions.route("inventory-service")
+                .route(RequestPredicates.path("/api/v1/inventory/venue/{venueId}"),
+                        request -> forwardWithPathVariable(request, "venueId",
+                                "http://localhost:8081/api/v1/inventory/venue/"))
+                .route(RequestPredicates.path("/api/v1/inventory/event/{eventId}"),
+                        request -> forwardWithPathVariable(request, "eventId",
+                                "http://localhost:8081/api/v1/inventory/event/"))
+                .build();
+    }
+
+    private ServerResponse forwardWithPathVariable(ServerRequest request, String pathVariable, String baseUrl) throws Exception {
+       String value = request.pathVariable(pathVariable);
+        return HandlerFunctions.http(baseUrl+value).handle(request);
+    }
+
+}
